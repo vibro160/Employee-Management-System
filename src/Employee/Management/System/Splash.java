638744f3e44123f21ef8f79a562c7f9a1d418cc1
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-07-19 02:22:09 +0530

    Commiting splash.java

--- src/Employee/Management/System/Splash.java
+++ src/Employee/Management/System/Splash.java
@@ -1,14 +1,31 @@
 package Employee.Management.System;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Splash extends JFrame {
 
      Splash(){
-        setSize(1680,820);
+         ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));
+         Image i2=i1.getImage().getScaledInstance(1200,820,Image.SCALE_DEFAULT );
+          ImageIcon i3=new ImageIcon(i2);
+          JLabel image =new JLabel(i3);
+          image.setBounds(0,0,1200,820);
+          add(image);
+
+
+
+        setSize(1200,820);
         setLayout(null);
+         setLocation(180,0);
         setVisible(true);
-       //  setLocation(0,0);
+        try {
+             Thread.sleep(5000);
+            setVisible(false);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 

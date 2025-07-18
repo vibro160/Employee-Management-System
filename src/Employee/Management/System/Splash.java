fd4850c3728e3f05642dcc21a5afc6e20404dbe2
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-07-19 00:26:55 +0530

    Commiting splash.java

---
+++ src/Employee/Management/System/Splash.java
@@ -0,0 +1,21 @@
+package Employee.Management.System;
+
+import javax.swing.*;
+
+public class Splash extends JFrame {
+
+     Splash(){
+        setSize(1680,820);
+        setLayout(null);
+        setVisible(true);
+       //  setLocation(0,0);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        new Splash();
+
+    }
+}
--- src/Employee/Management/System/splash.java
+++
@@ -1,7 +0,0 @@
-package Employee.Management.System;
-
-public class splash {
-    public static void main(String[] args) {
-//
-    }
-}

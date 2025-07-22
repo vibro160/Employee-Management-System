06b61e4133f651416b89db28d13f6dd2b08163dd
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-07-22 19:31:40 +0530

    Commiting splash.java

--- src/Employee/Management/System/addemployee.java
+++ src/Employee/Management/System/addemployee.java
@@ -1,12 +1,14 @@
 package Employee.Management.System;
 
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class addemployee  extends JFrame {
 
     JTextField employeename,fathername,dob,address,phoneno,aadhar,email,salary,designation;
-
+    JDateChooser tdob;
 
 
     addemployee() {
@@ -28,53 +30,79 @@ public class addemployee  extends JFrame {
         add(employeename);
 
         JLabel fname=new JLabel("Father's Name:");
-        fname.setBounds(400,200,150,30);
+        fname.setBounds(400,150,150,30);
         fname.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(fname);
         fathername=new JTextField();
-        fathername.setBounds(600,200,150,30);
+        fathername.setBounds(650,150,150,30);
         fathername.setBackground(new Color(177,252,197));
         add(fathername);
 
 
 
         JLabel phone=new JLabel("Phone No:");
-        phone.setBounds(50,300,150,30);
+        phone.setBounds(50,200,150,30);
         phone.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(phone);
         phoneno=new JTextField();
-        phoneno.setBounds(200,300,150,30);
+        phoneno.setBounds(200,200,150,30);
         phoneno.setBackground(new Color(177,252,197));
         add(phoneno);
 
         JLabel DOB=new JLabel(" DOB:");
-        DOB.setBounds(50,350,150,30);
+        DOB.setBounds(50,250,150,30);
         DOB.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(DOB);
-        dob=new JTextField();
-        dob.setBounds(200,350,150,30);
-       dob.setBackground(new Color(177,252,197));
-        add(dob);
+        tdob=new JDateChooser();
+        tdob.setBounds(200,250,150,30);
+       tdob.setBackground(new Color(177,252,197));
+        add(tdob);
 
         JLabel addres=new JLabel(" Address ");
-        addres.setBounds(50,250,150,30);
+        addres.setBounds(50,300,150,30);
         addres.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(addres);
         address=new JTextField();
-        address.setBounds(200,250,150,30);
+        address.setBounds(200,300,150,30);
         address.setBackground(new Color(177,252,197));
         add(address);
 
         JLabel adhar=new JLabel("Aadhar No:");
-        adhar.setBounds(50,400,150,30);
+        adhar.setBounds(50,350,150,30);
         adhar.setFont(new Font("SAN_SERIF",Font.BOLD,20));
         add(adhar);
         aadhar=new JTextField();
-        aadhar.setBounds(200,400,150,30);
+        aadhar.setBounds(200,350,150,30);
         aadhar.setBackground(new Color(177,252,197));
         add(aadhar);
 
 
+        JLabel sal=new JLabel("Salary:");
+        sal.setBounds(400,200,150,30);
+        sal.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(sal);
+        salary=new JTextField();
+        salary.setBounds(650,200,150,30);
+        salary.setBackground(new Color(177,252,197));
+        add(salary);
+
+        JLabel mail=new JLabel("Email ID:");
+        mail.setBounds(400,250,150,30);
+        mail.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(mail);
+        email=new JTextField();
+        email.setBounds(650,250,150,30);
+        email.setBackground(new Color(177,252,197));
+        add(email);
+
+        JLabel desg=new JLabel("Designation:");
+        desg.setBounds(400,300,150,30);
+        desg.setFont(new Font("SAN_SERIF",Font.BOLD,20));
+        add(desg);
+        designation=new JTextField();
+        designation.setBounds(650,300,150,30);
+        designation.setBackground(new Color(177,252,197));
+        add(designation);
 
 
         setSize(900, 700);

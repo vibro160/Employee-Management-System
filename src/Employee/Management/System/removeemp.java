e24cbf572500695b529c4bed6521b1a026d6d6ff
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-07-24 19:53:58 +0530

    Commiting splash.java

--- src/Employee/Management/System/removeemp.java
+++ src/Employee/Management/System/removeemp.java
@@ -1,4 +1,20 @@
 package Employee.Management.System;
 
-public class removeemp {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class removeemp extends JFrame implements ActionListener {
+    removeemp(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }

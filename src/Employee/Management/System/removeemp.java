04e43af6a8e98dc933c12d81542ce980a3bf933a
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-07-24 20:32:10 +0530

    Commiting splash.java

--- src/Employee/Management/System/removeemp.java
+++ src/Employee/Management/System/removeemp.java
@@ -1,11 +1,43 @@
 package Employee.Management.System;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 public class removeemp extends JFrame implements ActionListener {
+    Choice choiceempid;
+
+
+
+
+
     removeemp(){
+        JLabel lable= new JLabel("Employee ID");
+        lable.setBounds(50,50,150,30);
+        lable.setFont(new Font("Tahoma",Font.BOLD,20));
+        add(lable);
+        choiceempid=new Choice();
+        choiceempid.setBounds(250,50,150,30);
+        add(choiceempid);
+        try {
+            connection c= new connection();
+            String query="select * from employee";
+            ResultSet rs=c.statement.executeQuery(query);;
+            while (rs.next()){
+                choiceempid.add(rs.getString("empid"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        setSize(1000,300);
+        setLocation(300,150);
+        setVisible(true);
+        setLayout(null);
 
     }
 
@@ -15,6 +47,6 @@ public class removeemp extends JFrame implements ActionListener {
     }
 
     public static void main(String[] args) {
-
+    new removeemp();
     }
 }
